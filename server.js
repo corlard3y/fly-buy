1227a679b655cb5d354c9445db1e8fe7c86779fd
Kolade <corlardey@gmail.com>
2021-05-27 18:00:56 +0100

    prepare for deployment

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 // const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const shortid = require("shortid");
+const path = require('path');
 
 const app = express();
 app.use(express.json({extended: false}));
@@ -91,6 +92,16 @@ app.delete("/api/orders/:id", async (req, res) => {
   const order = await Order.findByIdAndDelete(req.params.id);
   res.send(order);
 });
+
+//serve static assets in production
+if(process.env.NODE_ENV === 'production'){
+  //set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client','build','index.html'));
+  });
+}
    
 
 const PORT = process.env.PORT || 5000;
